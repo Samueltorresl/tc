0fa5a4b26be7be46489ce30fc91a111e694e898d
Samuel <52602424+Samueltorresl@users.noreply.github.com>
2022-06-15 01:27:28 -0300

    Add files via upload

--- webscraping/funcaokdposit.py
+++ webscraping/funcaokdposit.py
@@ -1,17 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from lxml import html
 
-
-def kdspositivos():
-
-  req = requests.get(str('https://www.hltv.org/stats/players?startDate=2021-06-13&endDate=2022-06-13&rankingFilter=Top20'))
+datakd=""
+def kdspositivos(datakd):
+    
+  req = requests.get(str(datakd))
 
   kddif = re.findall(r'[+][0-9]+',req.text)
 
+  i = 1 
+
+  print('Jogador'+' '+'KD\n')
+
   for kdDiffCol  in kddif:
-    print(kdDiffCol)
- 
+        
+    tree = html.fromstring(req.content)
+
+    name_player = tree.xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tbody/tr[{}]/td[1]/a'.format(i))
+
+    print(name_player[0].text + ' ' + kdDiffCol,'\n')  
+    
+    i += 1 
 
 
 
--- webscraping/main.py
+++ webscraping/main.py
@@ -1,30 +1,139 @@
+import time
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import json
 from bs4 import BeautifulSoup
 import requests
 import re
+from lxml import html
 
-# FUNCOES 
+
+##FUNÇÕES
+
 def kdspositivos():
     
   req = requests.get(str(data))
 
+#expressao regular para encontrar os kds+ no req
   kddif = re.findall(r'[+][0-9]+',req.text)
 
-  nome_player = re.findall(r'<(.*?>(.*?)<\/\1', req.text)
-  print (nome_player)
+  i = 1 
 
+  print('\n''Jogador'+' '+'KD\n')
+
+#vai printar enquanto estiver na class kdDiffCol
   for kdDiffCol  in kddif:
-    print(kdDiffCol)
+        
+    tree = html.fromstring(req.content)
+
+#pega o xpath da tag e add 1 sempre que a lista atualizar  
+    name_player = tree.xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tbody/tr[{}]/td[1]/a'.format(i))
+
+    print(name_player[0].text, ' ->' + ' ' + kdDiffCol,'\n')  
+    
+    i += 1 
+ 
 
 def ratingall():
-
-    req = requests.get(str(data))
     
+    req = requests.get(str(data))
+
+#expressao regular que verifica os ratings dos jogadores   
     rating_geral = re.findall(r'<td class="ratingCol">(.+)</td>',req.text)
 
+    i = 1 
+
+    print('\n''Jogador'+'   '+'Rating\n')
+
     for ratingCol in rating_geral:
-        print(ratingCol)
+
+        tree = html.fromstring(req.content)
+
+#pega o padrao xpath da tag e add 1 sempre que a lista atualizar
+        name_player = tree.xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tbody/tr[{}]/td[1]/a'.format(i))
+
+        print(name_player[0].text, ' ->' + ' ' + ratingCol,'\n')
+        
+        i += 1
+
+def roundsall():
+
+    req = requests.get(str(data))
+
+#expressao regular para verificar os rounds ganhos
+    rounds_players = re.findall(r'<td class="statsDetail gtSmartphone-only">(.+)</td>',req.text)
+
+    i = 1
+
+    print('\n''Jogador'+'   '+'Rounds\n')
+
+    for Smartphoneonly in rounds_players:
+    
+        tree = html.fromstring(req.content)
+
+#pega o padrao xpath da tag e add 1 sempre que a lista atualizar
+        name_player = tree.xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tbody/tr[{}]/td[1]/a'.format(i))
+
+        print(name_player[0].text, ' ->' + ' ' + Smartphoneonly,'\n')
+        
+        i += 1
+
+def tabelacompleta():
+
+    print('Escolha como você deseja organizar sua tabela\n')
+    print('Organizar por mais mapas jogados --> 1\nOrganizar por mais rounds jogados --> 2\nOrganizar por maior KD --> 3\nOrganizar por maior Rating 1.0 --> 4')
+    menu = int(input())
+
+    option = Options()
+    option.headless = True
+    driver = webdriver.Firefox()
 
 
+    driver.get(data)
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]').click()
+    time.sleep(2)
+    # Organizar por mais mapas jogados
+    if menu == 1: 
+        driver.find_element_by_xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div/table/thead/tr/th[3]').click()
+    # Organizar por mais rounds jogados
+    if menu == 2:
+        driver.find_element_by_xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div/table/thead/tr/th[4]').click()
+    # Organizar por maior KD
+    if menu == 3:
+        driver.find_element_by_xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div/table/thead/tr/th[6]').click()
+    #Organizar por maior Rating 2.0
+    if menu == 4:
+        driver.find_element_by_xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div/table/thead/tr/th[7]').click()
+
+    time.sleep(1)
+    tabela = driver.find_element_by_xpath('/html/body/div[4]/div[1]/div[2]/div[1]/div/table')
+    html_content = tabela.get_attribute('outerHTML')
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    table = soup.find(name='table')
+
+    # Tranforma o html em uma tabela 
+    df_full = pd.read_html(str(table))[0].head(10)
+    df = df_full[['Player', 'Maps', 'Rounds', 'K/D', 'Rating2.0']]
+    df.columns = ['Jogador', 'Mapas', 'Rounds', 'K/D', 'Rating 2.0']
+
+    # Mostra o ranking na tela
+    print(df)
+
+    # Converte o conteudo em um dicionario
+    top10ranking = {}
+    top10ranking['Ranking'] = df.to_dict('records')
+
+    driver.quit()
+
+    js = json.dumps(top10ranking)
+    fp = open('ranking.json', 'w')
+    fp.write(js)
+    fp.close()
+
+###########################################################################################################################
 
 ## Pede a data para o usuário
 datauser = str(input("Digite a Data: \n"))
@@ -42,12 +151,12 @@ else:
     exit()
 
 #valida o link que o usuário vai colocar
-checkurl = str(input("Digite o link do top20\n"))
+checkurl = str(input("Digite o link do top20:\n"))
 
-
+#expressao regular que valida o link do usuário
 validador = re.search('\w.*.hltv.org.*players', checkurl)
 
-#substitui o valor que o usuário inseriu procurando pelo padrao da expressão
+#expressao regular que substitui a data do usuario 
 data = re.sub("(19|20)\d\d[- \.](0[1-9]|1[012])[- \.](0[1-9]|[12][0-9]|3[01])",datauser,checkurl, 1)
 
 #mesma coisa do if de cima, é um validador
@@ -59,8 +168,8 @@ else:
 
     exit()
 
-print('Digite 1 - Players com KD+\nDigite 2 - Rating de Todos os Players\nDigite 3 - Vizualizar a Tabela Por Completo\n-->')
-print(data)
+print('Digite 1 - Players com KD+\nDigite 2 - Rating de Todos os Players\nDigite 3 - Ver Quantos Rounds o Player Ganhou\nDigite 4 - Vizualizar a Tabela Por Completo''-->''\n')
+
 
 #funcionalidades do menu
 botao = int(input())
@@ -71,4 +180,13 @@ if botao == 1:
 
 if botao == 2:
     ratingall()
+
+if botao == 3:
+    roundsall()
+
+if botao == 4:
+    tabelacompleta()
+
+else:
+    print('Coloque um valor válido')
 
